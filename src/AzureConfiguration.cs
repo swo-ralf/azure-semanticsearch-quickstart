8f1d82871f361f273198278aced254a2f5e2744f
Ralf Ehlert <ralf.ehlert@softwareone.com>
2024-04-26 16:38:10 +0200

    Start Minimal WebAPI implementation

---
+++ src/AzureConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AzureSemanticSearchWebApi
+{
+    public class AzureConfiguration
+    {
+        public const string Key = "AzureConfiguration";
+
+        [Required]
+        public Uri ServiceEndpoint { get; set; } = null!;
+
+        [Required]
+        public string ApiKey { get; set; } = "";
+
+        [Required]
+        public string IndexName { get; set; } = "";
+    }
+}
---
+++ src/Endpoints/Admin/AdminEndpoints.cs
@@ -0,0 +1,12 @@
+﻿namespace AzureSemanticSearchWebApi.Endpoints.Admin
+{
+    public static class AdminEndpoints
+    {
+        public static WebApplication AddAdminEndpoints(this WebApplication app)
+        {
+            
+
+            return app;
+        }
+    }
+}
